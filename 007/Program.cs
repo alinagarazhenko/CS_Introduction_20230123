4610fa861adaa642c10984585734bca50e68e79a
alinagarazhenko <worldyachtphoto@ya.ru>
2023-02-02 17:34:59 +0300

    Lesson1 02

---
+++ 007/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить является ли число чётным.
+int N2=10;
+bool odd=N2%2==0;
+if (odd==true)
+{
+    System.Console.WriteLine($"{N2} четное");
+}
+else
+{
+ System.Console.WriteLine($"{N2} нечетное");   
+}
