bcf2b280c42961be98bdc74d458c6cbafd410799
alinagarazhenko <worldyachtphoto@ya.ru>
2023-02-08 12:02:03 +0300

    Lesson 3.01

---
+++ 033/Program.cs
@@ -0,0 +1,14 @@
+﻿// 33/ Возведите число А в натуральную степень B используя цикл
+
+int a=9;
+int b=2;
+int i=1;
+int result=a;
+
+while (i<b)
+{
+    result=result*a;
+    i++;
+}
+
+System.Console.WriteLine(result);
