769151fd800be83ec9bd2b80b6f0535f40ef59ec
alinagarazhenko <worldyachtphoto@ya.ru>
2023-02-08 14:21:48 +0300

    Lesson 3.04

---
+++ 036/Program.cs
@@ -0,0 +1,17 @@
+﻿// 36. Написать программу вычисления произведения чисел от 1 до N
+
+
+int a=1;
+int N;
+int multiplier=1;
+
+N=System.Convert.ToInt32(Console.ReadLine());
+ 
+for (int i=a; 
+         i<=N; 
+         i++) 
+{
+    multiplier*=i;
+}
+ 
+System.Console.WriteLine(multiplier);
