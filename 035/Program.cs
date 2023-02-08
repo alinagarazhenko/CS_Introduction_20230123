7622f9e90fee477bbd1b3aa84d4d1f68992390e0
alinagarazhenko <worldyachtphoto@ya.ru>
2023-02-08 13:49:47 +0300

    Lesson 3.03

--- 035/Program.cs
+++ 035/Program.cs
@@ -3,13 +3,13 @@
 
 int N=102354;
 int sumN=0;
-
+int i=10;
 
 while (N>0)
 {
-    int counter=N%10;
+    int counter=N%i;
     sumN=sumN+counter;
-    N=N/10;
+    N=N/i;
 }
 
 System.Console.WriteLine(sumN);
