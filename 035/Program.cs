87052efe2deff73f860cb798729ab366fa9d3b74
alinagarazhenko <worldyachtphoto@ya.ru>
2023-02-08 13:23:20 +0300

    Lesson 3 task 35 (почему-то не работает с числом 123501) Остальные варинаты считает правильно

---
+++ 035/Program.cs
@@ -0,0 +1,17 @@
+﻿// 34/ Подсчитать сумму цифр в числе. Сделать подпрограмму.
+
+
+
+int N=153501;
+int sumN=0;
+
+while (N>0)
+{
+    N=N/10;
+    int counter=N%10;
+    sumN=sumN+counter;
+    
+}
+
+System.Console.WriteLine(sumN);
+
