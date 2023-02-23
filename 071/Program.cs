1356edffd7a48e31245a1867caad74c96ddab42c
alinagarazhenko <worldyachtphoto@ya.ru>
2023-02-23 19:55:11 +0300

    Lesson 7.1

---
+++ 071/Program.cs
@@ -0,0 +1,13 @@
+﻿// 71. Показать натуральные числа от N до 1, N задано
+
+
+int N=20;
+string a(int i)
+{
+    if(i<=N) 
+        return a(i+1)+$"{i,4}";
+            else 
+                return String.Empty;
+}
+Console.WriteLine(a(1));
+
