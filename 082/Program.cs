f4da9a9db0fa22c32ef25434c70bfe81daad7cbc
alinagarazhenko <worldyachtphoto@ya.ru>
2023-02-27 19:17:39 +0300

    Lesson 8.2

---
+++ 082/Program.cs
@@ -0,0 +1,15 @@
+﻿// 82.C клавиатуры вводится строка разделенная точкой. Подсчитать количество символов до точки
+
+string s = Console.ReadLine();
+int k=0;
+
+System.Console.WriteLine();
+
+foreach(char c in s)
+{    if(c!='.') k++;
+    else break;
+}
+if(k==s.Length)
+    System.Console.WriteLine("Точка отсутствует");
+else
+    System.Console.WriteLine($"Символов до точки {k}");
---
+++ 084/Program.cs
@@ -0,0 +1,30 @@
+﻿// 84. Определить являются ли введенные с клавиатуры символы правильно записью целого числа. 
+// Вычислить сумму цифр введенного числа
+
+System.Console.WriteLine();
+string str=Console.ReadLine();
+char[] s1=str.ToCharArray();
+int k=0;
+
+for(int i=0;i<s1.Length;i++)
+{
+    if(char.IsDigit(s1[i])) k++; 
+}
+if(k==s1.Length)
+{
+    System.Console.WriteLine($"{str} целое число");
+    int digit = Int32.Parse(str);
+    System.Console.WriteLine(SumDigit(digit));
+}
+else System.Console.WriteLine("не целое число");
+
+int SumDigit(int a)
+{
+    int sum=0;
+    while(a>0)
+    {
+        sum+=a%10;
+        a/=10;
+    }
+    return sum;
+}
