15bf9489e23409427b7564b332dacea6ee59e50f
alinagarazhenko <worldyachtphoto@ya.ru>
2023-02-28 19:01:14 +0300

    Lesson 9

---
+++ 093/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 93: Задайте две матрицы. 
+// Напишите программу, которая будет находить произведение двух матриц.
+//а) простым поэлементным перемножением
+
+int[,] firstArray=Random2DArray(7,7);
+Print2DArray(firstArray);
+Console.WriteLine();
+
+int[,] secondArray=Random2DArray(7,7);
+Print2DArray(secondArray);
+Console.WriteLine();
+
+int[,] aArray=new int[firstArray.GetLength(0),secondArray.GetLength(1)];
+for(int i=0;i<aArray.GetLength(0);i++)
+{
+    for(int j=0;j<aArray.GetLength(1);j++)
+    {
+		for (int k=0;k<secondArray.GetLength(1);k++)
+		{
+        	aArray[i,j]+=firstArray[i,k]*secondArray[k,j];
+		}
+		System.Console.Write($"{aArray[i,j]} ");
+    }
+    System.Console.WriteLine();
+}
+
+
+
+
+void Print2DArray(int[,] arr)
+{
+	for(int i=0;i<arr.GetLength(0);i++)
+	{
+		for(int j=0;j<arr.GetLength(1);j++)
+		{
+			Console.Write($"{arr[i,j]} ");
+		}
+		Console.WriteLine();
+	}
+}
+
+
+int[,] Random2DArray(int row,int col)
+{
+    int[,] array2D=new int[row,col];
+    Random rand=new Random();
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0; j<col;j++)
+            array2D[i,j]=rand.Next(1,10);			
+    }
+    return array2D;
+}
---
+++ BasicAlgorthms/Generic/Program.cs
@@ -0,0 +1,40 @@
+﻿// Обобщения (Generic)
+
+List<string> list=new List<string>();
+List<int> list2=new List<int>();
+List<Random> List3=new List<Random>();
+
+class Example
+{
+
+static void Swap<T>(ref T a, ref T b) //шаблон с параметризированным аргументом
+{
+    T t=a;
+    a=b;
+    b=t;
+}
+// static void Swap(ref int a, ref int b)
+// {
+//     int t=a;
+//     a=b;
+//     b=t;    
+// }
+
+// static void Swap(ref double a, ref double b) //перегрузка по параметрам
+// {
+//     double t=a;
+//     a=b;
+//     b=t;        
+// }
+
+
+static void Main ()
+{
+    double x=3,y=5;
+
+    System.Console.WriteLine($"x={x} y={y}");
+    Swap(ref x, ref y);
+    System.Console.WriteLine($"x={x} y={y}");
+}
+
+}
